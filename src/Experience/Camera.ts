f081d068919586f4a3cb0be08ab0953a14bec73e
bb-33 <jonolj56@gmail.com>
2024-02-24 19:12:26 +0800

    change the view of the camera again

--- src/Experience/Camera.ts
+++ src/Experience/Camera.ts
@@ -27,7 +27,7 @@ export default class Camera
     setInstance()
     {
         this.instance = new PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 200)
-        this.instance.position.set(0, 0, 8)
+        this.instance.position.set(4, 6, 8)
         this.scene.add(this.instance)
     }
 
